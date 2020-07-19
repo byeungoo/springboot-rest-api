2944394d8d34698116306934f7da7d7fa2454641
ByeonGooHoon <dksekfldks65@naver.com>
2020-07-19 21:51:16 +0900

    Initial commit

---
+++ src/test/java/me/hoon/demoinflearnrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoon.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
