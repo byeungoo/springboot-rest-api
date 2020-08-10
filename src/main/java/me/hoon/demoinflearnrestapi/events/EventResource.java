ec7a9bbfc2d708f840534b846a08d73b3961b686
ByeonGooHoon <dksekfldks65@naver.com>
2020-08-10 23:56:10 +0900

    사용안하는 import 삭제

--- src/main/java/me/hoon/demoinflearnrestapi/events/EventResource.java
+++ src/main/java/me/hoon/demoinflearnrestapi/events/EventResource.java
@@ -3,9 +3,6 @@ package me.hoon.demoinflearnrestapi.events;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @AllArgsConstructor
 public class EventResource extends EntityModel<Event> {
